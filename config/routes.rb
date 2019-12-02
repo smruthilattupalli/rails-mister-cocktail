814431aa454da19b1e02a3ace833f61b5621febb
smruthi lattupalli <smruthireddy@gmail.com>
2019-12-02 13:10:33 +0100

    Added root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root to: 'cocktails#index'
   resources :doses, only: :destroy
   resources :cocktails, except: %i[edit update destroy] do
     resources :doses, only: %i[new create]
